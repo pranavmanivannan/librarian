86e5e97878357cb5d788417e0515536f59cef1aa
sriharshak27 <sriharshakocherla27@gmail.com>
2024-02-06 18:16:38 -0500

    huobi parser

--- src/listeners/huobi_listener.rs
+++ src/listeners/huobi_listener.rs
@@ -24,7 +24,78 @@ impl Listener for HuobiListener {
 
 impl Parser for HuobiParser {
     fn parse(message: Message) -> Result<DataPacket, ParseError> {
-        Err(ParseError::ParsingError)
+        let message_string = message.to_string();
+        let input_data: serde_json::Value =
+            serde_json::from_str(&message_string).map_err(ParseError::JsonError)?;
+
+        if !input_data.is_null() {
+            let parsed_data: &Value;
+            let ping_key: i64;
+
+            if let Some(ping_key) = input_data.get(ping) {
+                Ok(DataPacket::Ping(ping_key))
+            } else if Some(parsed_data) = &input_data["tick"] {
+                let data_type = parsed_data["event"];
+
+                let symbol_pair = parsed_data["ch"]
+                    .as_str()
+                    .ok_or(ParseError::ParsingError)?
+                    .split('.')
+                    .collect::<Vec<&str>>()
+                    .get(1)
+                    .ok_or(ParseError::ParsingError)?
+                    .to_uppercase();
+
+                let cur_seq = parsed_data["version"].as_i64().ok_or(ParseError::ParsingError)?;
+                let ts = parsed_data["ts"].as_i64().ok_or(ParseError::ParsingError)?;
+
+                let ask_vector = parsed_data["asks"]
+                    .as_array()
+                    .ok_or(ParseError::ParsingError)?;
+                let asks: Vec<Value> = if ask_vector.len() >= 5 {
+                    ask_vector[..5].to_vec()
+                } else {
+                    ask_vector.to_vec()
+                };
+
+                let bid_vector = parsed_data["bids"]
+                    .as_array()
+                    .ok_or(ParseError::ParsingError)?;
+                let bids: Vec<Value> = if bid_vector.len() >= 5 {
+                    bid_vector[..5].to_vec()
+                } else {
+                    bid_vector.to_vec()
+                };
+
+                if data_type == "incremental" {
+                    let enum_creator = MarketIncremental {
+                        symbol_pair,
+                        asks,
+                        bids,
+                        cur_seq,
+                        prev_seq: 0,
+                        timestamp: ts,
+                    };
+    
+                    Ok(DataPacket::MI(enum_creator))
+                } else {
+                    let enum_creator = Snapshot {
+                        symbol_pair,
+                        asks,
+                        bids,
+                        cur_seq,
+                        prev_seq: 0,
+                        timestamp: ts,
+                    };
+    
+                    Ok(DataPacket::ST(enum_creator))
+                }
+            } else {
+                Err(ParseError::ParsingError)
+            }
+        } else {
+            Err(ParseError::ParsingError)
+        }
     }
 }
 
@@ -48,7 +119,6 @@ impl SymbolHandler for HuobiSymbolHandler {
 
         let mut subscriptions: Vec<Value>;
         for symbol in symbol_pairs {
-
             // market incremental
             let inc_subscription = json!({
                 "sub": format!("market.{}.depth.size_5.high_freq", symbol.to_lowercase()),
